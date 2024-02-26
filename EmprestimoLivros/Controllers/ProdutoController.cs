5a032aabc94611af43114c357c58e3591396f70f
LeonardoVBrum <leonardobrum@yahoo.com>
2024-02-26 15:41:21 -0300

    primeirocommit

---
+++ EmprestimoLivros/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Produto()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        //CONSTRUTOR QUE PEGA AS OPTIONS DO DBCONTEXT
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+
+        // CRIAR TABELA NO BANCO
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+}
